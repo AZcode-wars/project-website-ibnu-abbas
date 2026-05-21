d855c9371fd5a199438a1436ae29719c284a016b
AZCode-wars <virzhaazura@gmail.com>
2026-05-21 10:26:24 +0700

    Setup Vercel Deployment2

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ibnu-abbas-website/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
